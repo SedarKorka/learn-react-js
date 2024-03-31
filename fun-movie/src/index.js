c5aed98d5fe8ab52771fa05cd99240be8cec10eb
Mamadou Korka Diallo <lelevekorka@gmail.com>
2024-03-31 23:54:56 +0200

    First organisation of my code

--- fun-movie/src/index.js
+++ fun-movie/src/index.js
@@ -1,8 +1,10 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
-import App from './App';
+import App from './components/App.js';
 import reportWebVitals from './reportWebVitals';
+
+import Home from './pages/Home/';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
